eaaaacec012dd4133acf6aad006d6700f72a2236
garrulero <90197951+garrulero@users.noreply.github.com>
2025-07-13 20:24:53 +0200

    Fix HTTPS protocol mismatch in config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const SERVER_CONFIG = {
   IP: 'localhost',
   PORT: 3001,
   get BASE_URL() {
-    return `http://${this.IP}:${this.PORT}`;
+    return `https://${this.IP}:${this.PORT}`;
   }
 };
