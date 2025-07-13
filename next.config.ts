048190d2c1c4d4cf8874a2699517fed75f2f99fe
garrulero <90197951+garrulero@users.noreply.github.com>
2025-07-13 19:38:03 +0200

    Corregir errores de configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: [
-      'https://9000-firebase-studio-1751918698415.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
